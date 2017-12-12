09e6627ef179af9d2a49f747ca3ba6463135b63b
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 16:06:27 +0100

    maj exo4

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
-<?php
-$text = 'Boulgour';
-$number = 69;
-$decimal = 42.55;
-$bool = true;
-echo $text.'<br/>';
-echo $number.'<br/>';
-echo $decimal.'<br/>';
-echo $bool.'<br/>';
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 4 partie 1 php</title>
+  </head>
+  <body>
+    <?php
+    $text = 'Boulgour';
+    $number = 69;
+    $decimal = 42.55;
+    $bool = true;
+    echo $text.'<br/>';
+    echo $number.'<br/>';
+    echo $decimal.'<br/>';
+    echo $bool.'<br/>';
+    ?>
+  </body>
+</html>
